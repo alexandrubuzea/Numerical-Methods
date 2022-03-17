7b909854af7bfe171e7cc261f1a94e629ee7f589
alexandrubuzea <alexandru.buzea2007@stud.acs.upb.ro>
2022-03-17 22:30:25 +0200

    lab 3 qr - for home

---
+++ for-home/03-qr/givens.m
@@ -0,0 +1,9 @@
+function [Q, R] = givens(A)
+  % scrieti o functie care realizeaza factorizarea ortogonala
+  % folosind rotori Givens.
+  % Functia va returna o matrice ortogonala si o matrice superior
+  % triunghiulara rezultate din factorizarea ortogonala.
+  
+  % TODO: Givens
+
+endfunction
